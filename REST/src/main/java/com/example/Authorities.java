d45878f3925383dd334c6b97aac29b26d6669b23
0LIVICH <tagir.nuretdinow@yandex.ru>
2025-08-02 14:38:09 +0300

    Delete REST directory

--- REST/src/main/java/com/example/Authorities.java
+++
@@ -1,5 +0,0 @@
-package com.example;
-
-public enum Authorities {
-    READ, WRITE, DELETE
-} 
--- REST/src/main/java/com/example/InvalidCredentials.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class InvalidCredentials extends RuntimeException {
-    public InvalidCredentials(String msg) {
-        super(msg);
-    }
-} 
